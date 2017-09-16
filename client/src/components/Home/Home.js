e8f9e20b0d823044fd08193e205df9db91c43273
Evelyn Sharp <yihongshi90@gmail.com>
2017-09-16 13:20:41 -0600

    header to parallax

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-import homeheader from '../../images/homeheader.jpeg';
+import home1 from '../../images/home1.jpeg';
+import home2 from '../../images/home2.jpeg';
 import glamorous from 'glamorous';
 
 const Parallax = glamorous.div({
-  backgroundImage: `url(${homeheader})`,
   height: '100vh',
   backgroundAttachment: 'fixed',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover'
+  backgroundSize: 'cover',
+})
 
+const HeaderMain = glamorous.h1({
+  color: 'white',
+  textAlign: 'center',
+  fontSize: '3em',
+  paddingTop: '1em !important',
 })
 
 class Home extends Component {
@@ -19,14 +25,28 @@ class Home extends Component {
 
     return(
       <div>
-        <Parallax></Parallax>
+        <Parallax style={{backgroundImage: `url(${home1})`}}>
+          <HeaderMain>home</HeaderMain>
+        </Parallax>
 
-        <div style={{ height: '100vh'}}>
+        <div style={{ height: '50vh'}}>
+        Scroll Up and Down this page to see the parallax scrolling effect.
+        This div is just here to enable scrolling.
+        Tip: Try to remove the background-attachment property to remove the scrolling effect.
+        </div>
+        <div style={{ backgroundColor:'black', height: '50vh'}}>
         Scroll Up and Down this page to see the parallax scrolling effect.
         This div is just here to enable scrolling.
         Tip: Try to remove the background-attachment property to remove the scrolling effect.
         </div>
 
+        <Parallax style={{backgroundImage: `url(${home2})`}}></Parallax>
+
+        <div style={{ height: '50vh'}}>
+        Scroll Up and Down this page to see the parallax scrolling effect.
+        This div is just here to enable scrolling.
+        Tip: Try to remove the background-attachment property to remove the scrolling effect.
+        </div>
       </div>
     );
   }
