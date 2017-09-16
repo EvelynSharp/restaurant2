11f4afcbbd915fb28547748b96effa011e4a18d6
Evelyn Sharp <yihongshi90@gmail.com>
2017-09-16 13:08:55 -0600

    parallax setup

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import homeheader from '../../images/homeheader.jpeg';
+import glamorous from 'glamorous';
+
+const Parallax = glamorous.div({
+  backgroundImage: `url(${homeheader})`,
+  height: '100vh',
+  backgroundAttachment: 'fixed',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover'
+
+})
 
 class Home extends Component {
+
   render() {
+
     return(
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <div>
+        <Parallax></Parallax>
+
+        <div style={{ height: '100vh'}}>
+        Scroll Up and Down this page to see the parallax scrolling effect.
+        This div is just here to enable scrolling.
+        Tip: Try to remove the background-attachment property to remove the scrolling effect.
+        </div>
+
+      </div>
     );
   }
 }
