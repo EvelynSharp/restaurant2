05e60c61965bdfcbfa6bcd15723a5f5893fc36c6
Evelyn Sharp <yihongshi90@gmail.com>
2017-10-02 16:33:58 -0600

    footer draft and icons

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import glamorous from 'glamorous';
+import { Icon, Button } from 'semantic-ui-react';
+import { PageHeader,
+         Content,
+         TextContainer,
+         PageLayerBase,
+         PageLayerBack,
+         PageHeaderImg,
+         ResHeader,
+        } from '../../styles/style-index';
+
+const ContentText = glamorous.div( Content, {
+  paddingTop: '1%',
+  color: 'white',
+})
+
+const Btn = glamorous.div({
+  color: 'white !important',
+  backgroundColor: 'rgba(0, 0, 0, 0) !important',
+  border: '2px solid white !important',
+  margin: '0 0.5em !important',
+  ':hover': {
+    color: '#3C342F !important',
+    backgroundColor: 'white !important',
+  },
+  '& i': {
+    fontSize: '1.2em !important'
+  }
+})
+
+const Footer = () => (
+  <div style={{backgroundColor: '#3C342F', color: 'white'}}>
+    <h1>Footer</h1>
+    <div style={{ padding: '1% 0 5% 0'}}>
+      <ContentText>{`1000 S 500 E `}</ContentText>
+      <ContentText>{`Salt Lake City, UT`}</ContentText>
+      <ContentText>{`84108`}</ContentText>
+      <ContentText>{`info@theeastslc.com`}</ContentText>
+    </div>
+    <div style={{ padding: '1% 0 5% 0'}}>
+      <Button as={Btn} icon='facebook f' />
+      <Button as={Btn} icon='instagram' />
+      <Button as={Btn} icon='twitter' />
+    </div>
+
+  </div>
+)
+
+export default Footer;
